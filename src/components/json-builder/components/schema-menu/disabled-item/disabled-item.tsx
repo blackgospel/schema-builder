fb5dd1927d20ed03d1b1cc65d5929cbc579bbbb4
oluseun.adesina <sadesina.96@gmail.com>
2022-05-10 12:05:04 +0100

    Added disabled field for root object

--- src/components/json-builder/components/schema-menu/disabled-item/disabled-item.tsx
+++ src/components/json-builder/components/schema-menu/disabled-item/disabled-item.tsx
@@ -1,10 +1,15 @@
 import { Form, Switch } from 'antd'
 import React, { PropsWithChildren } from 'react'
+import { setSchemaField } from 'src/helper/utils/schema'
 
 const DisabledItem = (props: PropsWithChildren<any>) => {
   return (
     <Form.Item name="suffix" noStyle>
-      <Switch size="small" {...props} />
+      <Switch
+        size="small"
+        {...props}
+        onClick={() => setSchemaField(props.item.value)}
+      />
     </Form.Item>
   )
 }
